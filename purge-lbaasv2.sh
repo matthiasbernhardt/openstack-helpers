50646fc57d94d339678ae743640c0ef15e5959fb
Matthias Bernhardt <40292398+matthiasbernhardt@users.noreply.github.com>
2022-07-27 08:27:59 +0200

    purge loadbalancer by project and use json/jq

--- purge-lbaasv2.sh
+++ purge-lbaasv2.sh
@@ -1,20 +1,34 @@
 #! /bin/bash
 
-#projectid=$1
-#loadbalancers=$(neutron lbaas-loadbalancer-list -f value 2>/dev/null | awk '$3 == "'$project'" {print $1}')
+if [[ "$1" == "-p" ]] ; then
+  shift
+  projects=("$@")
+  for project in ${projects[@]} ; do
+    project_json="$(openstack project show -f json $project)"
+    project_id="$(echo "$project_json" | jq -r ".id")"
+    project_name="$(echo "$project_json" | jq -r ".name")"
+    loadbalancer_list="$(neutron lbaas-loadbalancer-list -f json 2>/dev/null)"
+    loadbalancer_ids=($(echo "$loadbalancer_list" | jq -r '.[] | select(.tenant_id == "'"$project_id"'") | .id' ))
+  done
+else
+  loadbalancer_ids=("$@")
+fi
 
-loadbalancers=$*
-for loadbalancer in $loadbalancers ; do
-  echo "# loadbalancer $loadbalancer"
-  status="$(neutron lbaas-loadbalancer-status $loadbalancer 2>/dev/null)"
-  listeners=$(neutron lbaas-loadbalancer-show -f value -c listeners $loadbalancer 2>/dev/null | sed s+\'+\"+g | jq -r '.[].id' -r)
-  for listener in $listeners ; do
-    echo "neutron lbaas-listener-delete $listener"
+for loadbalancer_id in ${loadbalancer_ids[@]} ; do
+  loadbalancer_json="$(neutron lbaas-loadbalancer-show -f json $loadbalancer_id 2>/dev/null)"
+  loadbalancer_name="$(echo "$loadbalancer_json" | jq -r '.name' )"
+  loadbalancer_project_id="$(echo "$loadbalancer_json" | jq -r '.tenant_id' )"
+  echo "# loadbalancer '$loadbalancer_name' (id: $loadbalancer_id, project: $loadbalancer_project_id)"
+  loadbalancer_listeners="$(echo "$loadbalancer_json" | jq -r '.listeners' )"
+  loadbalancer_listener_ids=($(echo "$loadbalancer_listeners" | jq -r '.[].id' ))
+  for listener_id in ${loadbalancer_listener_ids[@]} ; do
+    echo "neutron lbaas-listener-delete $listener_id"
   done
-  pools=$(neutron lbaas-loadbalancer-show -f value -c pools $loadbalancer 2>/dev/null | sed s+\'+\"+g | jq -r '.[].id' -r)
-  for pool in $pools ; do
-    echo "neutron lbaas-pool-delete $pool"
+  loadbalancer_pools="$(echo "$loadbalancer_json" | jq -r '.pools' )"
+  loadbalancer_pool_ids=($(echo "$loadbalancer_pools" | jq -r '.[].id' ))
+  for pool_id in ${loadbalancer_pool_ids[@]} ; do
+    echo "neutron lbaas-pool-delete $pool_id"
   done
-  echo "neutron lbaas-loadbalancer-delete $loadbalancer"
+  echo "neutron lbaas-loadbalancer-delete $loadbalancer_id"
 done
 

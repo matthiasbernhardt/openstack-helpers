2af98806264d286420f2c3b3024015def8cadd51
Matthias Bernhardt <40292398+matthiasbernhardt@users.noreply.github.com>
2026-04-10 23:14:40 +0200

    sophisticated user and project selection supporting duplicate names

--- check-project-access.sh
+++ check-project-access.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 #
-# 2018…2021 M.Bernhardt, SysEleven GmbH, Berlin, Germany
+# 2018…2026 M.Bernhardt, SysEleven GmbH, Berlin, Germany
 #
 # show all users with direct or indirect relation to a (list of) given project(s)
 
@@ -9,30 +9,100 @@ if [ $BASH_VERSINFO -lt 4 ] ; then
   exit 1
 fi
 
-projects=("$@")
+projects_json="$(openstack project list --long -f json)"
+if [ -z "$projects_json" ] || ! [ "$(echo $projects_json | jq 'length')" -gt 0 ] ; then exit 1 ; fi
+match_projects() {
+  pattern="$1"
 
-declare -A id2role role2id
-#eval id2role=(`openstack role list -f value | sed -nEe 's/^([0-9a-z]{32}) ([-_a-zA-Z0-9]+)$/[\1]=\2/p'`)
-eval role2id=(`openstack role list -f value | sed -nEe 's/^([0-9a-z]{32}) ([-_a-zA-Z0-9]+)$/[\2]=\1/p'`)
-
-roleid_operator="${role2id[operator]}"
-roleid_viewer="${role2id[viewer]}"
-for project in ${projects[@]} ; do
-  projectinfo=($(openstack project show -f value -c id -c name $project))
-  projectid="${projectinfo[0]}"
-  projectname="${projectinfo[1]}"
-  echo "project: $projectname ($projectid)"
-  if [ -z "$projectid" ] ; then continue ; fi
-  userids="$(openstack role assignment list -f value -c User -c Role --project "$projectid" | awk  '$1=="'"${roleid_operator}"'" || $1=="'"${roleid_viewer}"'" { print $2 }'| sort | uniq | tr "\n" " " )"
-  echo "userids: $userids"
-  groupids="$(openstack role assignment list -f value -c Group -c Role --project "$projectid" | awk  '$1=="'"${roleid_operator}"'" || $1=="'"${roleid_viewer}"'" { print $2 }' | sort | uniq | tr "\n" " ")"
-  echo "groupids: $groupids"
-  if [ -n "$groupids" ] ; then
-    for groupid in $groupids ; do
-      groupname="$(openstack group show -f value -c name $groupid)"
-      echo "group: $groupname ($groupid)"
-      openstack user list --group $groupid
-    done
+  # exact match unique id
+  project_id="$(echo "$projects_json" | jq -r '.[] | select(.ID == "'"$pattern"'") | .ID' )"
+  if [ -n "$project_id" -a "$(echo "$project_id" | wc -l)" -eq 1 ] ; then
+    echo "$project_id"
+    return
   fi
+
+  # exact matches name
+  project_ids=($(echo "$projects_json" | jq -r '.[] | select(.Name == "'"$pattern"'") | .ID' ))
+
+  # exact matches description project_name (XYZ)
+  # "Description": "{\"organization_name\": \"org\", \"project_name\": \"prj\"}",
+  project_ids+=($(echo "$projects_json" | jq -r '.[] | select(."Domain ID" == "0123456789abcdef0123456789abcdef") | select(.Description | fromjson | .project_name=="'"$pattern"'") | .ID' ))
+
+  # exact matches description organization_name (XYZ)
+  project_ids+=($(echo "$projects_json" | jq -r '.[] | select(."Domain ID" == "0123456789abcdef0123456789abcdef") | select(.Description | fromjson | .organization_name=="'"$pattern"'") | .ID' ))
+
+  if [ -n "$project_ids" ] ; then
+    echo "${project_ids[@]}"
+    return
+  fi
+
+  # pattern matches name
+  project_ids=($(echo "$projects_json" | jq -r '.[] | select(.Name | match("'"$pattern"'")) | .ID' ))
+
+  # pattern matches description project_name (XYZ)
+  project_ids+=($(echo "$projects_json" | jq -r '.[] | select(."Domain ID" == "0123456789abcdef0123456789abcdef") | select(.Description | fromjson | .project_name | match("'"$pattern"'")) | .ID' ))
+
+  # pattern matches description organization_name (XYZ)
+  project_ids+=($(echo "$projects_json" | jq -r '.[] | select(."Domain ID" == "0123456789abcdef0123456789abcdef") | select(.Description | fromjson | .organization_name | match("'"$pattern"'")) | .ID' ))
+
+  if [ -n "$project_ids" ] ; then
+    echo "${project_ids[@]}"
+    return
+  fi
+
+  # pattern matches description
+  project_ids=($(echo "$projects_json" | jq -r '.[] | select(."Domain ID" == "0123456789abcdef0123456789abcdef") | select(.Description | match("'"$pattern"'")) | .ID' ))
+
+  if [ -n "$project_ids" ] ; then
+    echo "${project_ids[@]}"
+    return
+  fi
+
+  return 1
+}
+
+for query_pattern in "$@" ; do
+  if ! query_project_ids="$(match_projects "$query_pattern")" ; then
+    echo "no matches for '$query_pattern'"
+    continue
+  fi
+
+  for query_project_id in $query_project_ids ; do
+    if ! query_project_json="$(echo "$projects_json" | jq '.[] | select (.ID == "'"$query_project_id"'")' )" ; then continue ; fi
+    query_project_id="$(echo "$query_project_json" | jq -r ".ID")"
+    query_project_name="$(echo "$query_project_json" | jq -r ".Name")"
+    query_project_domain_id="$(echo "$query_project_json" | jq -r '."Domain ID"')"
+    query_project_description="$(echo "$query_project_json" | jq -r ".Description")"
+
+    if [ -z "$query_project_id" ] ; then continue ; fi
+
+    unset xyz abc filter_regions
+    if [[ "$query_project_id" == "$query_project_name" && "$query_project_domain_id" == "0123456789abcdef0123456789abcdef" ]] ; then
+      xyz=y # new cloud regions
+      query_project_info="$query_project_description"
+      filter_regions="(yyy|zzz)"
+    else
+      abc=y # old cloud regions
+      query_project_info="$query_project_name"
+      filter_regions="(aaa|bbb|ccc)"
+    fi
+
+    echo '```'
+    echo "# Project $query_project_id ($query_project_info)"
+
+    userids="$(openstack role assignment list -f value -c User -c Role --project "$query_project_id" | awk  '$1=="'"${roleid_operator}"'" || $1=="'"${roleid_viewer}"'" { print $2 }'| sort | uniq | tr "\n" " " )"
+    echo "userids: $userids"
+    groupids="$(openstack role assignment list -f value -c Group -c Role --project "$query_project_id" | awk  '$1=="'"${roleid_operator}"'" || $1=="'"${roleid_viewer}"'" { print $2 }' | sort | uniq | tr "\n" " ")"
+    echo "groupids: $groupids"
+    if [ -n "$groupids" ] ; then
+      for groupid in $groupids ; do
+        groupname="$(openstack group show -f value -c name $groupid)"
+        echo "group: $groupname ($groupid)"
+        openstack user list --group $groupid
+      done
+    fi
+
+    echo '```'
+  done
 done
 
--- check-user-access.sh
+++ check-user-access.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 #
-# 2018…2021 M.Bernhardt, SysEleven GmbH, Berlin, Germany
+# 2018…2026 M.Bernhardt, SysEleven GmbH, Berlin, Germany
 #
 # show all projects with direct or indirect relation to a (list of) given user(s)
 
@@ -9,76 +9,146 @@ if [ $BASH_VERSINFO -lt 4 ] ; then
   exit 1
 fi
 
-users=("$@")
+users_json="$(openstack user list --long -f json)"
+if [ -z "$users_json" ] || ! [ "$(echo $users_json | jq 'length')" -gt 0 ] ; then exit 1 ; fi
+match_users() {
+  pattern="$1"
 
-for user in ${users[@]} ; do
-  user_json="$(openstack user show -f json $user)"
-  user_id="$(echo "$user_json" | jq -r ".id")"
-  user_name="$(echo "$user_json" | jq -r ".name")"
-  user_enabled="$(echo "$user_json" | jq -er ".enabled")" || unset user_enabled
-  # if [ ! $? ] ; then echo unset user_enabled ; fi
-  default_project_id="$(echo "$user_json" | jq -er ".default_project_id")" || unset default_project_id
-  # if [ ! $? ] ; then unset default_project_id ; fi
-  echo "user: ${user_name} (${user_id}, enabled:${user_enabled})"
-
-  if [ -z "$user_name" -o -z "$user_id" ] ; then continue ; fi
-
-  if [ -n "$default_project_id" ] ; then
-    default_project_name="$(openstack project show -f value -c name $default_project_id)"
-    echo "default_project: $default_project_name ($default_project_id)"
-    echo "# ~/repos/openstack/s11stack-manager/client/purge-project.py --all-regions --keep-users --keep-groups --keep-project $default_project_id # $default_project_name"
-  else
-    echo "default_project: - (unset)"
+  # exact match unique id
+  user_id="$(echo "$users_json" | jq -r '.[] | select(.ID == "'"$pattern"'") | .ID' )"
+  if [ -n "$user_id" -a "$(echo "$user_id" | wc -l)" -eq 1 ] ; then
+    echo "$user_id"
+    return
   fi
 
-  echo "# openstack application credential list --user $user_name"
-  echo "# openstack credential list --user $user_name"
+  # exact matches name
+  user_ids=($(echo "$users_json" | jq -r '.[] | select(.Name == "'"$pattern"'") | .ID' ))
 
-  # TODO: ssh keys, tokens
-  # for region in ${regions[@]} ; do
-  #   openstack --os-region $region --os-username $user_id --os-password $password --os-project-id '' --os-project-name $projectname keypair list
-  # done
-
-  direct_projectids="$(openstack role assignment list -f value -c Project --user "$user_id" | sort | uniq | tr "\n" " ")"
-  echo "direct projectids: $direct_projectids"
-
-  declare -A groupids2names
-  eval groupids2names=($(openstack group list -f value --user "$user_id" | sed -nEe 's/^([0-9a-z]{32}) ([-+@_.a-zA-Z0-9]+)$/[\1]=\2/p'))
-  groupids="${!groupids2names[*]}"
-  echo "groupids: $groupids"
-  unset groups_projectids
-  if [ -n "$groupids" ] ; then
-    for groupid in $groupids ; do
-      groupname=${groupids2names[$groupid]}
-      echo "  group: $groupname ($groupid)"
-      group_member_count=$(openstack user list --group $groupid -f value | wc -l)
-      if [ $group_member_count -le 1 ] ; then
-        echo "    # openstack group remove user $groupname $user_name # POTENTIALLY ABANDONED"
-        echo "    # openstack user list --group $groupid # $groupname"
-      else
-        echo "    # openstack group remove user $groupname $user_name"
-      fi
-      projectids="$(openstack role assignment list -f value -c Project --group "$groupid" | sort | uniq | tr "\n" " ")"
-      echo "  group projectids: $projectids"
-      groups_projectids+="$projectids"
-    done
-  fi
-
-  all_projectids=$(echo $default_project_id $direct_projectids $groups_projectids | tr " " "\n" | sort | uniq | tr "\n" " ")
-  echo "all projectids: $all_projectids"
-  for project_id in $all_projectids ; do
-    project_json="$(openstack project show -f json $project_id)"
-    project_id="$(echo "$project_json" | jq -r ".id")"
-    project_name="$(echo "$project_json" | jq -r ".name")"
-    project_parent_id="$(echo "$project_json" | jq -r ".parent_id")"
-    project_description="$(echo "$project_json" | jq -r ".description")"
-
-    if [[ "$project_id" == "$project_name" ]] ; then
-      # syseleven-openstack-cloud / Keystone domain for customer projects (NCS)
-      project_name="$project_description"
+  # exact matches email
+  new_user_ids=$(echo "$users_json" | jq -r '.[] | select(.Email == "'"$pattern"'") | .ID' )
+  for user_id in $new_user_ids ; do
+    if ! echo "${user_ids[@]}" | xargs -n 1 | egrep  "^$user_id\$" > /dev/null ; then
+      user_ids+=($user_id)
     fi
-    echo "  project: $project_name ($project_id)"
   done
 
-done
+  # exact matches description
+  new_user_ids=$(echo "$users_json" | jq -r '.[] | select(.Description == "'"$pattern"'") | .ID' )
+  for user_id in $new_user_ids ; do
+    if ! echo "${user_ids[@]}" | xargs -n 1 | egrep  "^$user_id\$" > /dev/null ; then
+      user_ids+=($user_id)
+    fi
+  done
 
+  if [ -n "$user_ids" ] ; then
+    echo "${user_ids[@]}"
+    return
+  fi
+
+  # pattern matches name
+  user_ids=($(echo "$users_json" | jq -r '.[] | select(.Name | match("'"$pattern"'")) | .ID' ))
+
+  # pattern matches email
+  new_user_ids=$(echo "$users_json" | jq -r '.[] | select(.Email) | select (.Email | match("'"$pattern"'")) | .ID' )
+  for user_id in $new_user_ids ; do
+    if ! echo "${user_ids[@]}" | xargs -n 1 | egrep  "^$user_id\$" > /dev/null ; then
+      user_ids+=($user_id)
+    fi
+  done
+
+  # pattern matches description
+  new_user_ids=$(echo "$users_json" | jq -r '.[] | select(.Description) | select (.Description | match("'"$pattern"'")) | .ID' )
+  for user_id in $new_user_ids ; do
+    if ! echo "${user_ids[@]}" | xargs -n 1 | egrep  "^$user_id\$" > /dev/null ; then
+      user_ids+=($user_id)
+    fi
+  done
+
+  if [ -n "$user_ids" ] ; then
+    echo "${user_ids[@]}"
+    return
+  fi
+
+  return 1
+}
+
+for query_pattern in "$@" ; do
+  if ! query_user_ids="$(match_users "$query_pattern")" ; then
+    echo "no matches for '$query_pattern'"
+    continue
+  fi
+
+  for query_user_id in $query_user_ids ; do
+    if ! query_user_json="$(echo "$users_json" | jq '.[] | select (.ID == "'"$query_user_id"'")' )" ; then continue ; fi
+    query_user_id="$(echo "$query_user_json" | jq -r ".ID")"
+    query_user_name="$(echo "$query_user_json" | jq -r ".Name")"
+    query_user_domain_id="$(echo "$query_user_json" | jq -r '."Domain"')"
+    query_user_description="$(echo "$query_user_json" | jq -r ".Description")" || unser query_user_description
+    query_user_project_id="$(echo "$query_user_json" | jq -re ".Project")" || unset query_user_project_id
+    query_user_email="$(echo "$query_user_json" | jq -re ".Email")" || unset query_user_email
+    query_user_enabled="$(echo "$query_user_json" | jq -re ".Enabled")" || unset query_user_enabled
+
+    echo "user: ${query_user_name} (${query_user_id}, enabled:${query_user_enabled})"
+
+    if [ -z "$query_user_name" -o -z "$query_user_id" ] ; then continue ; fi
+
+    if [ -n "$query_user_project_id" ] ; then
+      default_project_name="$(openstack project show -f value -c name $query_user_project_id)"
+      echo "default_project: $default_project_name ($query_user_project_id)"
+      echo "# ~/repos/openstack/s11stack-manager/client/purge-project.py --all-regions --keep-users --keep-groups --keep-project $query_user_project_id # $default_project_name"
+    else
+      echo "default_project: - (unset)"
+    fi
+
+    echo "# openstack application credential list --user $query_user_name"
+    echo "# openstack credential list --user $query_user_name"
+
+    # TODO: ssh keys, tokens
+    # for region in ${regions[@]} ; do
+    #   openstack --os-region $region --os-username $query_user_id --os-password $password --os-project-id '' --os-project-name $projectname keypair list
+    # done
+
+    direct_projectids="$(openstack role assignment list -f value -c Project --user "$query_user_id" | sort | uniq | tr "\n" " ")"
+    echo "direct projectids: $direct_projectids"
+
+    declare -A groupids2names
+    eval groupids2names=($(openstack group list -f value --user "$query_user_id" | sed -nEe 's/^([0-9a-z]{32}) ([-+@_.a-zA-Z0-9]+)$/[\1]=\2/p'))
+    groupids="${!groupids2names[*]}"
+    echo "groupids: $groupids"
+    unset groups_projectids
+    if [ -n "$groupids" ] ; then
+      for groupid in $groupids ; do
+        groupname=${groupids2names[$groupid]}
+        echo "  group: $groupname ($groupid)"
+        group_member_count=$(openstack user list --group $groupid -f value | wc -l)
+        if [ $group_member_count -le 1 ] ; then
+          echo "    # openstack group remove user $groupname $query_user_name # POTENTIALLY ABANDONED"
+          echo "    # openstack user list --group $groupid # $groupname"
+        else
+          echo "    # openstack group remove user $groupname $query_user_name"
+        fi
+        projectids="$(openstack role assignment list -f value -c Project --group "$groupid" | sort | uniq | tr "\n" " ")"
+        echo "  group projectids: $projectids"
+        groups_projectids+="$projectids"
+      done
+    fi
+
+    all_projectids=$(echo $query_user_project_id $direct_projectids $groups_projectids | tr " " "\n" | sort | uniq | tr "\n" " ")
+    echo "all projectids: $all_projectids"
+    for project_id in $all_projectids ; do
+      project_json="$(openstack project show -f json $project_id)"
+      project_id="$(echo "$project_json" | jq -r ".id")"
+      project_name="$(echo "$project_json" | jq -r ".name")"
+      project_parent_id="$(echo "$project_json" | jq -r ".parent_id")"
+      project_description="$(echo "$project_json" | jq -r ".description")"
+
+      if [[ "$project_id" == "$project_name" ]] ; then
+        # syseleven-openstack-cloud / Keystone domain for customer projects (NCS)
+        project_name="$project_description"
+      fi
+      echo "  project: $project_name ($project_id)"
+    done
+
+  done # query_user_id
+
+done # query_pattern
